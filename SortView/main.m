22d493860b0147e646d3e3abb11bbddab726460b
Will Han <xingheng907@hotmail.com>
2014-10-06 06:46:35 +0800

    Initial Commit

---
+++ SortView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SortView
+//
+//  Created by WeiHan on 10/6/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SortViewTests/SortViewTests.m
@@ -0,0 +1,34 @@
+//
+//  SortViewTests.m
+//  SortViewTests
+//
+//  Created by WeiHan on 10/6/14.
+//  Copyright (c) 2014 Wei Han. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SortViewTests : XCTestCase
+
+@end
+
+@implementation SortViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
